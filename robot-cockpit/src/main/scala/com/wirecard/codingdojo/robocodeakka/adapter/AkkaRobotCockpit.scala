8b84ba2dbe2a8dd81fe6b55bc26b4435f1d2ecca
Fabio Oliveira <fabio.braga@gmail.com>
2016-09-24 20:23:01 +0200

    Added configurable remote pilots, configured them to connect to localhost

--- robot-cockpit/src/main/scala/com/wirecard/codingdojo/robocodeakka/adapter/AkkaRobotCockpit.scala
+++ robot-cockpit/src/main/scala/com/wirecard/codingdojo/robocodeakka/adapter/AkkaRobotCockpit.scala
@@ -3,7 +3,7 @@ package com.wirecard.codingdojo.robocodeakka.adapter
 import akka.pattern.ask
 import akka.util.Timeout
 import com.wirecard.codingdojo.robocodeakka.messages._
-import robocode.{BattleEndedEvent, Robot, ScannedRobotEvent}
+import robocode.{BattleEndedEvent, Robot, RoundEndedEvent, ScannedRobotEvent}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -13,12 +13,16 @@ import scala.concurrent.duration._
   */
 class AkkaRobotCockpit() extends Robot {
 
-  val ref = AkkaRobotPilotRef.ref("akka.tcp://AkkaRobotPilotSystem@192.168.0.19:2552/user/robopilot")
+  val ref = AkkaRobotPilotRef.ref()
 
   override def run() = {
     while (true) {
       triggerEvent(NextCommand())
     }
+  }
+
+  override def onScannedRobot(e: ScannedRobotEvent) = {
+    triggerEvent(ScannedRobot(e.getName, e.getEnergy, e.getHeading, e.getBearing, e.getDistance, e.getVelocity, e.isSentryRobot))
   }
 
   def triggerEvent(message: RobotEvents): Unit = {
@@ -33,13 +37,13 @@ class AkkaRobotCockpit() extends Robot {
     }
   }
 
-  override def onScannedRobot(e: ScannedRobotEvent) = {
-    triggerEvent(ScannedRobot(e.getName, e.getEnergy, e.getHeading, e.getBearing, e.getDistance, e.getVelocity, e.isSentryRobot))
-  }
-
   override def onBattleEnded(e: BattleEndedEvent) = {
     ref ! BattleEnded()
+    AkkaRobotPilotRef.reset()
+  }
 
+  override def onRoundEnded(e: RoundEndedEvent) = {
+    AkkaRobotPilotRef.reset()
   }
 
 }
--- robot-cockpit/src/main/scala/com/wirecard/codingdojo/robocodeakka/adapter/AkkaRobotPilotRef.scala
+++ robot-cockpit/src/main/scala/com/wirecard/codingdojo/robocodeakka/adapter/AkkaRobotPilotRef.scala
@@ -1,6 +1,7 @@
 package com.wirecard.codingdojo.robocodeakka.adapter
 
 import akka.actor.{ActorSelection, ActorSystem}
+import com.typesafe.config.{Config, ConfigFactory}
 
 /**
   * @author <a href="mailto:fabio.oliveira@wirecard.com">Fabio Oliveira</a>
@@ -8,8 +9,19 @@ import akka.actor.{ActorSelection, ActorSystem}
 object AkkaRobotPilotRef {
 
   val system = ActorSystem()
+  private val config: Config = ConfigFactory.load();
+  private var configIdx: Int = 0
 
-  def ref(addr: String): ActorSelection = system.actorSelection(addr)
+  def ref(): ActorSelection = {
+    val addrs = config.getStringList("cockpit.pilots")
+    val addr = addrs.get(configIdx)
+    configIdx = configIdx + 1
+
+    system.actorSelection(addr)
+  }
+
+  def reset(): Unit = configIdx = 0
+
 
 
 }
